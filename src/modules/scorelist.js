64178c66fc536901c28fac13b67d4b8fa7134ee3
Debabrata Mandal <bapidebabrata@gmail.com>
2022-06-16 08:35:42 +0530

    Improve user experience while loading data from server

--- src/modules/scorelist.js
+++ src/modules/scorelist.js
@@ -1,9 +1,11 @@
 import ScoreBoard from './scoreboard.js';
+import displayUI from './initializeUI.js'
 
 const scoreList = {};
 
 (async () => {
   scoreList.list = await ScoreBoard.createScoreBoard('New Game by NayanKaran1');
+  displayUI();
 })();
 
 export default scoreList;
